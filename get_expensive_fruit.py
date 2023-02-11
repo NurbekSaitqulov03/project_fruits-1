bb83e217438c89af2d0c52f93837383eaf1ffcb0
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-11 21:45:06 +0500

    get_expensive_fruit.py

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -1,3 +1,4 @@
+import csv
 def get_expensive_fruit(data: str) -> str:
     """
     This function returns the name of the most expensive fruit
@@ -8,6 +9,17 @@ def get_expensive_fruit(data: str) -> str:
         str: name of the most expensive fruit
     """
     # your code here
-    return  
+    data = csv.reader(data, delimiter=',')
+    a = []
+    for i in data:
+        if not i[1].isalpha():
+            a.append(float(i[1]))
+    mx = a[0]
+    for i in a[1:]:
+        if i>mx:
+            mx = i
+    return mx
 
 
+f = open('fruits.csv', 'r')
+print(get_expensive_fruit(f))
